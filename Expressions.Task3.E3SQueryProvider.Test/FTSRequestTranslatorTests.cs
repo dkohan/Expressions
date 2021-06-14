07809e79e0e22457ced5be148738f312b21b38fa
Dzmitriy Kakhanouski <dzmitriy_kakhanouski@epam.com>
2021-06-14 17:24:40 +0300

    first commit

---
+++ Expressions.Task3.E3SQueryProvider.Test/FTSRequestTranslatorTests.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Linq;
+using System.Linq.Expressions;
+using Expressions.Task3.E3SQueryProvider.Models.Entities;
+using Xunit;
+
+namespace Expressions.Task3.E3SQueryProvider.Test
+{
+    public class FtsRequestTranslatorTests
+    {
+        #region SubTask 1 : operands order
+
+        [Fact]
+        public void TestBinaryBackOrder()
+        {
+            var translator = new ExpressionToFtsRequestTranslator();
+            Expression<Func<EmployeeEntity, bool>> expression
+                = employee => "EPBYMINW0834" == employee.Workstation;
+
+            string translated = translator.Translate(expression);
+            Assert.Equal("Workstation:(EPBYMINW0834)", translated);
+        }
+
+        #endregion
+
+        #region SubTask 2: inclusion operations
+
+        [Fact]
+        public void TestBinaryEqualsQueryable()
+        {
+            var translator = new ExpressionToFtsRequestTranslator();
+            Expression<Func<IQueryable<EmployeeEntity>, IQueryable<EmployeeEntity>>> expression
+                = query => query.Where(e => e.Workstation == "EPBYMINW0834");
+
+            string translated = translator.Translate(expression);
+            Assert.Equal("Workstation:(EPBYMINW0834)", translated);
+        }
+
+        [Fact]
+        public void TestBinaryEquals()
+        {
+            var translator = new ExpressionToFtsRequestTranslator();
+            Expression<Func<EmployeeEntity, bool>> expression
+                = employee => employee.Workstation == "EPBYMINW0834";
+
+            string translated = translator.Translate(expression);
+            Assert.Equal("Workstation:(EPBYMINW0834)", translated);
+        }
+
+        [Fact]
+        public void TestMethodEquals()
+        {
+            var translator = new ExpressionToFtsRequestTranslator();
+            Expression<Func<EmployeeEntity, bool>> expression
+                = employee => employee.Workstation.Equals("EPBYMINW0834");
+
+            string translated = translator.Translate(expression);
+            Assert.Equal("Workstation:(EPBYMINW0834)", translated);
+        }
+
+        [Fact]
+        public void TestStartsWith()
+        {
+            var translator = new ExpressionToFtsRequestTranslator();
+            Expression<Func<EmployeeEntity, bool>> expression
+                = employee => employee.Workstation.StartsWith("EPBYMINW0834");
+            
+            string translated = translator.Translate(expression);
+            Assert.Equal("Workstation:(EPBYMINW0834*)", translated);
+        }
+
+        [Fact]
+        public void TestEndsWith()
+        {
+            var translator = new ExpressionToFtsRequestTranslator();
+            Expression<Func<EmployeeEntity, bool>> expression
+                = employee => employee.Workstation.EndsWith("EPBYMINW0834");
+
+            string translated = translator.Translate(expression);
+            Assert.Equal("Workstation:(*EPBYMINW0834)", translated);
+        }
+
+        [Fact]
+        public void TestContains()
+        {
+            var translator = new ExpressionToFtsRequestTranslator();
+            Expression<Func<EmployeeEntity, bool>> expression
+                = employee => employee.Workstation.Contains("EPBYMINW0834");
+
+            string translated = translator.Translate(expression);
+            Assert.Equal("Workstation:(*EPBYMINW0834*)", translated);
+        }
+
+        #endregion
+    }
+}
---
+++ Expressions.Task3.E3SQueryProvider/ExpressionToFTSRequestTranslator.cs
@@ -0,0 +1,136 @@
+﻿using System;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+
+namespace Expressions.Task3.E3SQueryProvider
+{
+    public class ExpressionToFtsRequestTranslator : ExpressionVisitor
+    {
+        readonly StringBuilder _resultStringBuilder;
+
+        public ExpressionToFtsRequestTranslator()
+        {
+            _resultStringBuilder = new StringBuilder();
+        }
+
+        public string Translate(Expression exp)
+        {
+            Visit(exp);
+
+            return _resultStringBuilder.ToString();
+        }
+
+        #region protected methods
+
+        protected override Expression VisitMethodCall(MethodCallExpression node)
+        {
+            if (node.Method.DeclaringType == typeof(Queryable)
+                && node.Method.Name == "Where")
+            {
+                var predicate = node.Arguments[1];
+                Visit(predicate);
+
+                return node;
+            }
+            else if (node.Method.DeclaringType == typeof(string)
+                 && node.Method.Name == "Contains")
+            {
+                var argument = node.Arguments[0];
+
+                if (argument.NodeType == ExpressionType.Constant)
+                {
+                    Visit(node.Object);
+                    _resultStringBuilder.Append("(*");
+                    Visit(argument);
+                    _resultStringBuilder.Append("*)");
+
+                }
+                else
+                {
+                    Visit(argument);
+                }
+
+                return node;
+            }
+            else if (node.Method.Name == "StartsWith")
+            {
+                var predicate = node.Arguments[0];
+                Visit(node.Object);
+                _resultStringBuilder.Append("(");
+                Visit(predicate);
+                _resultStringBuilder.Append("*)");
+                return node;
+            }
+            else if (node.Method.Name == "EndsWith")
+            {
+                var predicate = node.Arguments[0];
+                Visit(node.Object);
+                _resultStringBuilder.Append("(*");
+                Visit(predicate);
+                _resultStringBuilder.Append(")");
+                return node;
+            }
+            else if (node.Method.Name == "Equals")
+            {
+                var predicate = node.Arguments[0];
+                Visit(node.Object);
+                _resultStringBuilder.Append("(");
+                Visit(predicate);
+                _resultStringBuilder.Append(")");
+                return node;
+            }
+
+            return base.VisitMethodCall(node);
+        }
+
+        protected override Expression VisitBinary(BinaryExpression node)
+        {
+            switch (node.NodeType)
+            {
+                case ExpressionType.Equal:
+
+                    var member = node.Right;
+                    var constant = node.Left;
+
+                    if(node.Left.NodeType != ExpressionType.Constant)
+                    {
+                        member = node.Left;
+                        constant = node.Right;
+                    }
+
+                    Visit(member);
+                    _resultStringBuilder.Append("(");
+                    Visit(constant);
+                    _resultStringBuilder.Append(")");
+                    break;
+                case ExpressionType.AndAlso:
+                    Visit(node.Left);
+                    _resultStringBuilder.Append(" AND ");
+                    Visit(node.Right);
+                    break;
+
+                default:
+                    throw new NotSupportedException($"Operation '{node.NodeType}' is not supported");
+            };
+
+            return node;
+        }
+
+        protected override Expression VisitMember(MemberExpression node)
+        {
+            _resultStringBuilder.Append(node.Member.Name).Append(":");
+
+            return base.VisitMember(node);
+        }
+
+        protected override Expression VisitConstant(ConstantExpression node)
+        {
+            _resultStringBuilder.Append(node.Value);
+
+            return node;
+        }
+
+        #endregion
+    }
+}
